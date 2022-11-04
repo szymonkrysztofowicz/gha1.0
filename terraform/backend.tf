1d4030dd91b4932460230cb72081c0f5d5ba5128
szymon.krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2022-11-04 18:20:02 +0100

    fix

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket = "test"
     key    = "test"
-    region = "us-east-1"
-    dynamodb_table = "tf-state-236117720236-us-east-1"
-    profile = "default"
+    region = "test"
+    dynamodb_table = "test"
+    profile = "test"
   }
 }
