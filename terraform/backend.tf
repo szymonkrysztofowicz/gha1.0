afe525f98892ae9d6717e03ba887dc1bb140a60f
szymon.krysztofowicz <szymon.krysztofowicz@nordcloud.com>
2022-11-04 18:18:29 +0100

    fix

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "arn:aws:s3:::tfstatebucketsk2022"
-    key    = "tfstate"
+    bucket = "test"
+    key    = "test"
     region = "us-east-1"
     dynamodb_table = "tf-state-236117720236-us-east-1"
     profile = "default"
